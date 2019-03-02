b23acd383117607498b861f314e8f9d32590ef35
jjaesang <jaesang2443@naver.com>
2019-03-02 20:05:14 +0900

    RemoveDuplicatedFromSortedarray LinkedList

--- src/main/easy/RemoveDuplicatedFromSortedListTree.java
+++ src/main/easy/RemoveDuplicatedFromSortedListTree.java
@@ -2,9 +2,8 @@ package main.easy;
 
 /**
  * Given a sorted linked list, delete all duplicates such that each element appear only once.
- *
+ * <p>
  * https://leetcode.com/problems/remove-duplicates-from-sorted-list/
- *
  */
 
 public class RemoveDuplicatedFromSortedListTree {
@@ -12,6 +11,7 @@ public class RemoveDuplicatedFromSortedListTree {
     public class ListNode {
         int val;
         ListNode next;
+
         ListNode(int x) {
             val = x;
         }
@@ -20,11 +20,11 @@ public class RemoveDuplicatedFromSortedListTree {
     public ListNode deleteDuplicates(ListNode head) {
         ListNode current = head;
         while (current != null && current.next != null) {
-            if(current.val == current.next.val){
+            if (current.val == current.next.val)
                 current.next = current.next.next;
-            }else
+            else
                 current = current.next;
         }
-        return current;
+        return head;
     }
 }
