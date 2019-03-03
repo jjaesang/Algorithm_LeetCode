3a5ae509d9c4e5326e3562c4d4b837920dd6cfd0
jjaesang <jaesang2443@naver.com>
2019-03-03 22:51:32 +0900

    MaxProfit_Linear TIme

---
+++ src/main/easy/MaxProfit.java
@@ -0,0 +1,50 @@
+package main.easy;
+
+/**
+ *
+ * Say you have an array for which the ith element is the price of a given stock on day i.
+ * If you were only permitted to complete at most one transaction (i.e., buy one and sell one share of the stock), design an algorithm to find the maximum profit.
+ * Note that you cannot sell a stock before you buy one.
+ *
+ * https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+ */
+public class MaxProfit {
+
+    /**
+     * Time O(n2) / Space O(1)
+     * @param prices [7,1,5,3,6,4]
+     * @return 5
+     *
+     * Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+    Not 7-1 = 6, as selling price needs to be larger than buying price.
+     */
+    public int maxProfit_on2(int prices[]) {
+        int maxprofit = 0;
+        for (int i = 0; i < prices.length - 1; i++) {
+            for (int j = i + 1; j < prices.length; j++) {
+                int profit = prices[j] - prices[i];
+                if (profit > maxprofit)
+                    maxprofit = profit;
+            }
+        }
+        return maxprofit;
+    }
+
+    /**
+     * Time O(n) / Space O(1)
+     * @param prices [7,6,4,3,1]
+     * @return 0
+     * Explanation: In this case, no transaction is done, i.e. max profit = 0.
+     */
+    public int maxProfit_on(int prices[]) {
+        int minprice = Integer.MAX_VALUE;
+        int maxprofit = 0;
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] < minprice)
+                minprice = prices[i];
+            else if (prices[i] - minprice > maxprofit)
+                maxprofit = prices[i] - minprice;
+        }
+        return maxprofit;
+    }
+}
