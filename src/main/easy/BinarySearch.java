138cad82cc8f96658d084d3521ba78d09ed72223
임재상 <jaesang@zuminternet.com>
2019-04-03 19:41:13 +0900

    BinarySearch 문제

--- src/main/easy/BinarySearch.java
+++ src/main/easy/BinarySearch.java
@@ -11,12 +11,11 @@ public class BinarySearch {
     /**
      * Runtime: 0 ms, faster than 100.00% of Java online submissions for Binary Search.
      * Memory Usage: 40.3 MB, less than 89.92% of Java online submissions for Binary Search.
-     * @param nums
-     * @param target
-     * @return
+     * @param nums {-1, 0, 3, 5, 9, 12}
+     * @param target 0
+     * @return 4
      */
     public static int search(int[] nums, int target) {
-
         int start = 0;
         int end = nums.length -1;
         while (start <= end) {
