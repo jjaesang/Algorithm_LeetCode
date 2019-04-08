64c098f22fd38da3d84325d6f54c169b0c0c6660
임재상 <jaesang@zuminternet.com>
2019-04-08 19:12:14 +0900

    70. Climbing Stairs

--- src/main/easy/ClimbStairs.java
+++ src/main/easy/ClimbStairs.java
@@ -3,12 +3,18 @@ package main.easy;
 /**
  * DP Problems
  * You are climbing a stair case. It takes n steps to reach to the top.
-
- Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
-
- Note: Given n will be a positive integer.
+ *  Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top
+ *   Note: Given n will be a positive integer.
+ *
+ *   https://leetcode.com/problems/climbing-stairs/
  */
 public class ClimbStairs {
+    /**
+     * Runtime: 0 ms, faster than 100.00% of Java online submissions for Climbing Stairs.
+     * Memory Usage: 31.8 MB, less than 100.00% of Java online submissions for Climbing Stairs.
+     * @param n
+     * @return
+     */
     public int climbStairs(int n) {
         if( n == 1)
             return 1;
