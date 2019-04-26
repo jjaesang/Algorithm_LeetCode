35c748074b45785836e331e3c791db041f241c62
jaesang <jaesang2443@naver.com>
2019-04-26 21:11:49 +0900

    350. Intersection of Two Arrays II ( hashMap & sort )

--- src/main/easy/IntersectOfTwoArray.java
+++ src/main/easy/IntersectOfTwoArray.java
@@ -1,9 +1,6 @@
 package main.easy;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 /**
  * Given two arrays, write a function to compute their intersection.
@@ -15,14 +12,10 @@ import java.util.Set;
 public class IntersectOfTwoArray {
 
     /**
-     * Input: nums1 = [1,2,2,1], nums2 = [2,2]
-     * Output: [2,2]
-     *
-     * @param nums1
-     * @param nums2
-     * @return
+     * Runtime: 3 ms, faster than 57.39% of Java online submissions for Intersection of Two Arrays II.
+     * Memory Usage: 38.2 MB, less than 7.39% of Java online submissions for Intersection of Two Arrays II.
      */
-    public static int[] intersect(int[] nums1, int[] nums2) {
+    public static int[] intersectMap(int[] nums1, int[] nums2) {
 
         ArrayList<Integer> result = new ArrayList<>();
         HashMap<Integer, Integer> hashMap = new HashMap<>();
@@ -46,16 +39,42 @@ public class IntersectOfTwoArray {
     }
 
     /**
-     * Runtime: 3 ms, faster than 57.39% of Java online submissions for Intersection of Two Arrays II.
-     * Memory Usage: 38.2 MB, less than 7.39% of Java online submissions for Intersection of Two Arrays II.
-     * @param args
+     * Runtime: 2 ms, faster than 90.40% of Java online submissions for Intersection of Two Arrays II.
+     * Memory Usage: 37.4 MB, less than 41.80% of Java online submissions for Intersection of Two Arrays II.
+     * @param nums1
+     * @param nums2
+     * @return
      */
+    public static int[] intersectSort(int[] nums1, int[] nums2) {
+
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+
+        int i=0;
+        int j=0;
+        ArrayList<Integer> result = new ArrayList<>();
+        while(i<nums1.length && j<nums2.length){
+            if(nums1[i] == nums2[j]){
+                result.add(nums1[i]);
+                i++; j++;
+            }
+            else if (nums1[i] < nums2[j])
+                i++;
+            else
+                j++;
+        }
+
+        int[] ret = new int[result.size()];
+        for( int k =0 ;k< result.size() ;k++)
+            ret[k] = result.get(k);
+        return ret;
+    }
+
     public static void main(String[] args) {
+        int[] nums1 = {1,2,2,1};
+        int[] nums2 = {2,2};
 
-        int[] nums1 = {1,2};
-        int[] nums2 = {1,1};
-
-        for( int re : intersect(nums1,nums2))
+        for( int re : intersectSort(nums1,nums2))
             System.out.println(re);
 
     }
